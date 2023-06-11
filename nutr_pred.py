6a461c2ecdcf728fbb35fea2f6ecdd774ff84492
anedanman <anedanman@gmail.com>
2023-06-11 22:53:16 +0300

    NutrPredText model

--- nutr_pred.py
+++ nutr_pred.py
@@ -4,6 +4,8 @@ from torch import nn
 import pytorch_lightning as pl
 from torchvision.utils import make_grid
 
+from transformers import AutoModel
+
 from encoder import Encoder
 
 
@@ -41,28 +43,29 @@ class NutrPred(pl.LightningModule):
         self.lr = lr
         self.loss_function = nn.L1Loss()
 
-    def forward(self, x):
+    def forward(self, batch):
+        x = batch['image']
         x = self.encoder(x)
         x = self.mlp(x)
         return x
     
     def training_step(self, batch, batch_idx):
-        x, y = batch['image'], batch['nutr_info']
-        pred = self(x)
+        y = batch['nutr_info']
+        pred = self(batch)
         loss = self.loss_function(pred, y)
         self.log('train_loss', loss)
         return loss
     
     def validation_step(self, batch, batch_idx):
-        x, y = batch['image'], batch['nutr_info']
-        pred = self(x)
+        y = batch['nutr_info']
+        pred = self(batch)
         loss = self.loss_function(pred, y)
         self.log('val_loss', loss)
         return loss
     
     def test_step(self, batch, batch_idx):
-        x, y = batch['image'], batch['nutr_info']
-        pred = self(x)
+        y = batch['nutr_info']
+        pred = self(batch)
         loss = self.loss_function(pred, y)
         self.log('test_loss', loss)
         return loss
@@ -70,6 +73,42 @@ class NutrPred(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
         return optimizer
+    
+
+class NutrPredText(NutrPred):
+    def __init__(self, 
+                in_channels=3, 
+                hidden_channels=64, 
+                downsample_steps_before=4, 
+                conv_steps=1, 
+                downsample_steps_after=2,
+                dropout=0.20,
+                mlp_hidden=128,
+                lr=0.0003):
+        super().__init__(in_channels, hidden_channels, downsample_steps_before, conv_steps, downsample_steps_after, dropout, mlp_hidden, lr)
+        self.text_encoder = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        self.mlp = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(2 * 2 * hidden_channels + 384, mlp_hidden),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_hidden, mlp_hidden),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_hidden, 5)
+        )
+
+    @torch.no_grad()
+    def encode_text(self, text):
+        return self.text_encoder(text)[0].mean(dim=1)
+    
+    def forward(self, batch):
+        x, text = batch['image'], batch['text']
+        x = self.encoder(x)
+        text = self.encode_text(text)
+        x = torch.cat([x, text], dim=-1)
+        x = self.mlp(x)
+        return x
 
 
 class NutrientsLogger(pl.Callback):
@@ -78,11 +117,11 @@ class NutrientsLogger(pl.Callback):
         x, y = val_batch['image'], val_batch['nutr_info']
         self.x = x[:num_samples]
         self.y = y[:num_samples]
-
+        self.batch = val_batch
 
     def on_validation_epoch_end(self, trainer, pl_module):
         images = self.x
-        pred_nutr_info = pl_module(images.to(pl_module.device))
+        pred_nutr_info = pl_module(self.batch)
         true_nutr_info = self.y
 
         columns = ['Input image', 'Prediction', 'Expected']
